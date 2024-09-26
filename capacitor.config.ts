a029d777d36345681e284373db595772544e1730
ricvvrdoo <rica.rodriguezs@duocuc.com>
2024-09-25 21:30:05 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ProyectoLuxor',
+  webDir: 'dist'
+};
+
+export default config;
